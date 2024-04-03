d490a90724d8e169ddeb27e24f85648acb52d88a
Aymeric FELTEN <afelten@paul-f4Ar5s9.clusters.42paris.fr>
2024-04-03 18:12:25 +0200

    delete

--- transcendence/transcendence/back/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('back/', views.back, name='back'),
-    path('users/', views.user_list, name='user_list'),
-    path('users/<int:pk>/', views.user_detail, name='user_detail'),
-]
