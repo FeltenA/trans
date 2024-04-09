a38b11cd3607a69c38e120777fadad2f98cee3e2
Aymeric FELTEN <afelten@paul-f5Ar4s4.clusters.42paris.fr>
2024-04-09 18:45:58 +0200

    modif_docker

--- back/tools/script.sh
+++ back/tools/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sed -i -r "s/your_db_name/$POSTGRES_DB/1"   ./transcendence/settings.py
-sed -i -r "s/your_db_user/$POSTGRES_USER/1"   ./transcendence/settings.py
-sed -i -r "s/your_db_password/$POSTGRES_PASSWORD/1"   ./transcendence/settings.py
+sed -i -r "s/your_db_name/$POSTGRES_DB/1"   /transcendence/transcendence/settings.py
+sed -i -r "s/your_db_user/$POSTGRES_USER/1"   /transcendence/transcendence/settings.py
+sed -i -r "s/your_db_password/$POSTGRES_PASSWORD/1"   /transcendence/transcendence/settings.py
 
-python transcendence/manage.py runserver 0.0.0.0:8000
+python /transcendence/manage.py migrate
+python /transcendence/manage.py runserver 0.0.0.0:8000
